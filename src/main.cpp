0e86994a03daa86f7ff40284cf67861574526472
Himansh Jain <himanshjain@Himanshs-MacBook-Pro.local>
2024-09-09 13:05:46 +0530

    second stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
+#include <bits/stdc++.h>
 #include <iostream>
+
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  vector<string> permissibleCommands;
+
   // Uncomment this block to pass the first stage
   std::cout << "$ ";
   
   std::string input;
   std::getline(std::cin, input);
+
+  if(find(permissibleCommands.begin(), permissibleCommands.end(), input) == permissibleCommands.end()) {
+	  cout << input << ": command not found" << endl;
+  }
+
 }
