c04eb041e47bd4adc3aafdaf4a527945a2fac0b7
Himansh Jain <mail.himanshjain@gmail.com>
2025-11-26 02:55:00 +0530

    Implemented echo

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,27 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <sstream>
+
 
 using namespace std;
+
+std::vector<std::string> splitString(const std::string& s, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
 
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    vector<string> permissibleCommands = {"exit"};
+    vector<string> permissibleCommands = {"exit", "echo"};
 
     // Uncomment this block to pass the first stage
 
@@ -19,13 +31,27 @@ int main() {
         std::cout << "$ ";
         std::getline(std::cin, input);
 
-        if (find(permissibleCommands.begin(), permissibleCommands.end(), input) == permissibleCommands.end()) {
+        vector<string> tokens = splitString(input, ' ');
+
+        if (tokens.empty() || (tokens[0] != "echo" && find(permissibleCommands.begin(), permissibleCommands.end(), input) == permissibleCommands.end())) {
             cout << input << ": command not found" << endl;
         }
+
+        if (tokens.empty()) continue;
 
         if (input == "exit") {
             break;
         }
+
+        const string& command = tokens[0];
+
+        if (command == "echo") {
+            for (int i=1; i<tokens.size(); i++) {
+                cout << tokens[i] << " ";
+            }
+            cout << endl;
+        }
+
     }
 
     return 0;
